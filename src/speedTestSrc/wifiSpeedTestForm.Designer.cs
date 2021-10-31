1c81a4ce1d0fd46caffa3adf7a3ab1dd0239c08a
Mat <up2011287@myport.ac.uk>
2021-10-31 19:53:55 +0000

    Debugging and added new form

--- src/speedTestSrc/wifiSpeedTestForm.Designer.cs
+++ src/speedTestSrc/wifiSpeedTestForm.Designer.cs
@@ -29,35 +29,99 @@ namespace Network_Detective.src.speedTestSrc
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(wifiSpeedTestForm));
-            this.button1 = new System.Windows.Forms.Button();
+            this.startSpeedTestButton = new System.Windows.Forms.Button();
+            this.currentSpeedtextBox = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.testNetInter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // startSpeedTestButton
             // 
-            this.button1.BackColor = System.Drawing.Color.LawnGreen;
-            this.button1.Location = new System.Drawing.Point(111, 197);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = false;
+            this.startSpeedTestButton.BackColor = System.Drawing.Color.LawnGreen;
+            this.startSpeedTestButton.Location = new System.Drawing.Point(99, 254);
+            this.startSpeedTestButton.Name = "startSpeedTestButton";
+            this.startSpeedTestButton.Size = new System.Drawing.Size(75, 23);
+            this.startSpeedTestButton.TabIndex = 0;
+            this.startSpeedTestButton.Text = "Start";
+            this.startSpeedTestButton.UseVisualStyleBackColor = false;
+            this.startSpeedTestButton.Click += new System.EventHandler(this.startSpeedTestButton_Click);
+            // 
+            // currentSpeedtextBox
+            // 
+            this.currentSpeedtextBox.Location = new System.Drawing.Point(12, 12);
+            this.currentSpeedtextBox.Multiline = true;
+            this.currentSpeedtextBox.Name = "currentSpeedtextBox";
+            this.currentSpeedtextBox.ReadOnly = true;
+            this.currentSpeedtextBox.Size = new System.Drawing.Size(258, 100);
+            this.currentSpeedtextBox.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(292, 38);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // testNetInter
+            // 
+            this.testNetInter.BackColor = System.Drawing.Color.LawnGreen;
+            this.testNetInter.Location = new System.Drawing.Point(12, 347);
+            this.testNetInter.Name = "testNetInter";
+            this.testNetInter.Size = new System.Drawing.Size(75, 23);
+            this.testNetInter.TabIndex = 3;
+            this.testNetInter.Text = "Try";
+            this.testNetInter.UseVisualStyleBackColor = false;
+            this.testNetInter.Click += new System.EventHandler(this.testNetInter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Test All Network Interfaces:";
             // 
             // wifiSpeedTestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(676, 382);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.testNetInter);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.currentSpeedtextBox);
+            this.Controls.Add(this.startSpeedTestButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "wifiSpeedTestForm";
             this.Text = "Network Detective (WIFI Speed Test)";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button startSpeedTestButton;
+        private System.Windows.Forms.TextBox currentSpeedtextBox;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button testNetInter;
+        private System.Windows.Forms.Label label1;
     }
 }
--- src/speedTestSrc/wifiSpeedTestForm.cs
+++ src/speedTestSrc/wifiSpeedTestForm.cs
@@ -2,19 +2,79 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+
 
 namespace Network_Detective.src.speedTestSrc
 {
     public partial class wifiSpeedTestForm : Form
     {
+        private volatile bool threadFlag_Abort;
+        private volatile float seconds;
+
         public wifiSpeedTestForm()
         {
             InitializeComponent();
+        }
+
+        private void startSpeedTestButton_Click(object sender, EventArgs e)
+        {
+            threadFlag_Abort = false;
+
+            Thread timer_THREAD0 = new Thread(timer_THREAD);
+            currentSpeedtextBox.Clear();
+            timer_THREAD0.Start();
+
+            WebClient webClient = new WebClient();
+
+            Uri URL = new Uri("http://ipv4.download.thinkbroadband.com:81/20MB.zip");
+            while (!threadFlag_Abort)
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    wc.Headers.Add("User-Agent: Other");
+                    wc.DownloadFile(URL, "wifi_TEST_TEMP.zip");
+                    threadFlag_Abort = true;
+                    timer_THREAD0.Abort();
+                }
+            }
+
+            String wifiFile = "wifi_TEST_TEMP.zip";
+            long FileSize = wifiFile.Length;
+
+            double speedInBytes = FileSize / seconds;
+
+            double speedInMegaBits = (speedInBytes / 8) / 8 ;
+            //currentSpeedtextBox.Text += string.Format("{0:0.#}Mbps", speedInMegaBits);
+            currentSpeedtextBox.Text += string.Format("{0}", speedInMegaBits);
+
+
+        }
+        private void timer_THREAD()
+        {
+            Stopwatch downloadTimer = new Stopwatch();
+            downloadTimer.Reset();
+            downloadTimer.Start();
+            while (seconds < 60)
+            {
+                seconds = (float)downloadTimer.Elapsed.TotalSeconds;
+            }
+            threadFlag_Abort = true;
+            downloadTimer.Stop();
+
+            seconds = (float)downloadTimer.Elapsed.TotalSeconds;
+        }
+
+        private void testNetInter_Click(object sender, EventArgs e)
+        {
+            testAllNetworkInterfaces testAllNetworkInterfacesFrom = new testAllNetworkInterfaces();
+            testAllNetworkInterfacesFrom.Show();
         }
     }
 }
