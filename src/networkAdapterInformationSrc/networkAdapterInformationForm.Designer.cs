df8662fab86e8d5f70aa993567e5cee2664cbae1
Mat <up2011287@myport.ac.uk>
2021-11-03 18:48:24 +0000

    added NetworkAdapt

--- src/networkAdapterInformationSrc/networkAdapterInformationForm.Designer.cs
+++ src/networkAdapterInformationSrc/networkAdapterInformationForm.Designer.cs
@@ -30,13 +30,23 @@ namespace Network_Detective
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(networkAdapterInformationForm));
+            this.mainListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // mainListBox
+            // 
+            this.mainListBox.FormattingEnabled = true;
+            this.mainListBox.Location = new System.Drawing.Point(12, 12);
+            this.mainListBox.Name = "mainListBox";
+            this.mainListBox.Size = new System.Drawing.Size(552, 420);
+            this.mainListBox.TabIndex = 0;
             // 
             // networkAdapterInformationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainListBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "networkAdapterInformationForm";
             this.Text = "Network Detective (Network Adapter Infomation)";
@@ -45,5 +55,7 @@ namespace Network_Detective
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox mainListBox;
     }
 }
--- src/networkAdapterInformationSrc/networkAdapterInformationForm.cs
+++ src/networkAdapterInformationSrc/networkAdapterInformationForm.cs
@@ -4,9 +4,19 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
+//https://docs.microsoft.com/en-us/dotnet/api/system.net.networkinformation.networkinterface?view=net-5.0
+
+
+/// <summary>
+///  KEY NOTE:               Fix index error if computer does not have IPv6 Addresses
+/// </summary>
 
 namespace Network_Detective
 {
@@ -15,6 +25,89 @@ namespace Network_Detective
         public networkAdapterInformationForm()
         {
             InitializeComponent();
+            onLoad();
+        }
+
+        private void onLoad()
+        {
+            var ifcs = NetworkInterface.GetAllNetworkInterfaces();
+
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            String mainTitle = $"Interface information for {computerProperties.HostName}.{computerProperties.DomainName}";
+            mainListBox.Items.Add(mainTitle);
+            if (nics == null || nics.Length < 1)
+            {
+                mainListBox.Items.Add("  No network interfaces found.");
+                return;
+            }
+            mainListBox.Items.Add($"  Number of interfaces .................... : {nics.Length}");
+            foreach (NetworkInterface adapter in nics)
+            {
+                
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+                
+                mainListBox.Items.Add(adapter.Description);
+                mainListBox.Items.Add(String.Empty.PadLeft(adapter.Description.Length, '='));
+                mainListBox.Items.Add($"  Interface type .......................... : {adapter.NetworkInterfaceType}");
+                mainListBox.Items.Add($"  Speed  .................................... :{adapter.Speed} bits per second");
+                //mainListBox.Items.Add($"{adapter.GetIPProperties()}");
+                mainListBox.Items.Add($"IP {adapter.GetIPProperties().UnicastAddresses[1].Address.ToString()}");
+                mainListBox.Items.Add($"IP {adapter.GetIPProperties().UnicastAddresses[0].Address.ToString()}");
+                mainListBox.Items.Add($"  Physical Address ........................ : {adapter.GetPhysicalAddress().ToString()}");
+                mainListBox.Items.Add($"  Operational status ...................... : {adapter.OperationalStatus}");
+                string versions = "";
+
+
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    versions = "IPv4";
+                }
+                if (adapter.Supports(NetworkInterfaceComponent.IPv6))
+                {
+                    if (versions.Length > 0)
+                    {
+                        versions += " ";
+                    }
+                    versions += "IPv6";
+                }
+                mainListBox.Items.Add($"  IP version .............................. : {versions}");
+
+                // The following information is not useful for loopback adapters.
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                {
+                    continue;
+                }
+                mainListBox.Items.Add($"  DNS suffix .............................. : {properties.DnsSuffix}");
+
+                string label;
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    IPv4InterfaceProperties ipv4 = properties.GetIPv4Properties();
+                    mainListBox.Items.Add($"  MTU...................................... : {ipv4.Mtu}");
+                    if (ipv4.UsesWins)
+                    {
+
+                        IPAddressCollection winsServers = properties.WinsServersAddresses;
+                        if (winsServers.Count > 0)
+                        {
+                            label = "  WINS Servers ............................ :";
+
+                        }
+                    }
+                }
+
+                mainListBox.Items.Add($"  DNS enabled ............................. : {properties.IsDnsEnabled}");
+                mainListBox.Items.Add($"  Dynamically configured DNS .............. : {properties.IsDynamicDnsEnabled}");
+                mainListBox.Items.Add($"  Receive Only ............................ : {adapter.IsReceiveOnly}");
+                mainListBox.Items.Add($"  Multicast ............................... : {adapter.SupportsMulticast}");
+                //ShowInterfaceStatistics(adapter);
+
+                mainListBox.Items.Add("");
+            }
+            
+
+
         }
     }
 }
